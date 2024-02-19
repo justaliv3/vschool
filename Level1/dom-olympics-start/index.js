dc66f697f0fa071876ddedf7c68d046204a7c1ad
Justin Brown <justalive@gmail.com>
2024-02-19 14:53:04 -0700

    Olympics project

---
+++ Level1/dom-olympics-start/index.js
@@ -0,0 +1,24 @@
+var header = document.getElementById("header").append("JavaScript Made This!!"); 
+document.getElementById("header").style.fontWeight = "900";
+var span = document.getElementById("name").style.color = "peachpuff";
+
+document.getElementById("right1").textContent = "something good";
+document.querySelector("#left1").textContent = "Hello World";
+
+var messages1 = document.getElementById("right1");
+var messages2 = document.getElementById("left1");
+var messages3 = document.getElementById("right2");
+var messages4 = document.getElementById("left2");
+
+document.getElementById("clear-button").onclick = function(){
+    messages1.remove();
+    messages2.remove();
+    messages3.remove();
+    messages4.remove();
+}
+
+
+
+
+
+
