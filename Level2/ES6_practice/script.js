a847e06e0d076dbafa769668f0a7b324100b70a6
justaliv3 <justalive@gmail.com>
2024-03-26 16:22:14 -0600

    Es6 Practice

---
+++ Level2/ES6_practice/script.js
@@ -0,0 +1,22 @@
+const name = "John"
+const age = 101
+
+function runForLoop(pets) {
+    let petObjects = []
+    for (var i = 0; i < pets.length; i++) {
+        let pet = { type: pets[i] }
+        let name;
+        if (pets[i] === "cat") {
+            name = "fluffy"
+        } else {
+            name = "spot"
+        }
+        console.log("pet name: ", name)
+        pet.name = name
+        petObjects.push(pet)
+    }
+    console.log("man name: ", name)
+    return petObjects
+}
+
+runForLoop(["cat", "dog"])
